4020b042bb27d18377f391b7ee74417bb93fe0bb
AJ Mcgladdery and Colin Cook <colin@>
2018-08-15 13:23:35 -0700

    Add spec for non-alpha characters in pigLatin.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+//Back-end logic
+var pigLatin = function(userInput) {
+  return userInput;
+}
+
+
+//Front-end logic
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
+    var userInput = $("#userInput").val();
+    var result = pigLatin(userInput);
+    $("#finalOutput").text(result);
     $("#results").show();
+
   });
 });
